d57768300cab21438b71c65366579ce7ffbfc870
Saransh <saranshlingwal923@gmail.com>
2022-10-19 01:08:38 +0530

    some tests

--- app.js
+++ app.js
@@ -1,23 +1,27 @@
 //jshint esversion:6
 const express = require("express");
+const path = require('path');
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 
 
 const homeStartingContent = "Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.";
-const aboutContent = "Hac habitasse platea dictumst vestibulum rhoncus est pellentesque. Dictumst vestibulum rhoncus est pellentesque elit ullamcorper. Non diam phasellus vestibulum lorem sed. Platea dictumst quisque sagittis purus sit. Egestas sed sed risus pretium quam vulputate dignissim suspendisse. Mauris in aliquam sem fringilla. Semper risus in hendrerit gravida rutrum quisque non tellus orci. Amet massa vitae tortor condimentum lacinia quis vel eros. Enim ut tellus elementum sagittis vitae. Mauris ultrices eros in cursus turpis massa tincidunt dui.";
-
+const aboutContent = " kdajsfkj dksfajdks jkdjf kajd jdafkljds; fkja;ds  lkjdaf kjdska jfac habitasse platea dictumst vestibulum rhoncus est pellentesque. Dictumst vestibulum rhoncus est pellentesque elit ullamcorper. Non diam phasellus vestibulum lorem sed. Platea dictumst quisque sagittis purus sit. Egestas sed sed risus pretium quam vulputate dignissim suspendisse. Mauris in aliquam sem fringilla. Semper risus in hendrerit gravida rutrum quisque non tellus orci. Amet massa vitae tortor condimentum lacinia quis vel eros. Enim ut tellus elementum sagittis vitae. Mauris ultrices eros in cursus turpis massa tincidunt dui.";
+let footerContent = "kdajsfkj dksfajdks jkdjf kajd jdafkljds; fkja;ds  lkjdaf kjdska jfac habitasse platea dictumst vestibulum rhoncus est pellentesque. Dictumst vestibulum rhoncus est pellentesque elit ullamcorper. Non diam phasellus vestibulum lorem sed. Platea dictumst quisque sagittis purus sit. Egestas sed sed risus pretium quam vulputate dignissim suspendisse. Mauris in aliquam sem fringilla. Semper risus in hendrerit gravida rutrum quisque non tellus orci. Amet massa vitae tortor condimentum lacinia quis vel eros. Enim ut tellus elementum sagittis vitae. Mauris ultrices eros in cursus turpis massa tincidunt dui.";
 const app = express();
+
 app.set('view engine' ,'ejs');
+app.set('views' ,path.join(__dirname,'views'));
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use(express.static("public")); // to load css
+app.use(express.static(path.join(__dirname,'public'))); // to load css
 
 app.get("/" , function(req , res){
     res.render("home" , {hStartingContent : homeStartingContent})
 });
 
 app.get("/about" , function(req , res){
-    res.render("about" , {aContent : aboutContent})
+    res.render("about" , {aContent : aboutContent ,
+    fContent : footerContent})
 })
 
 
